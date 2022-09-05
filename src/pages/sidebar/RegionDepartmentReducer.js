1ab888ca65c6ba54604c0b89df2fb3ff330e05af
bobur017 <bobur.m.a017@gmail.com>
2022-09-05 10:33:02 +0500

    щл

---
+++ src/pages/sidebar/RegionDepartmentReducer.js
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { setApi } from "./ApiCall";
+import {apiCall} from "../ApiCall";
+const slice = createSlice({
+    name: "reducer",
+    initialState: {
+        result: {},
+        error: {},
+        department:[],
+    },
+    reducers: {
+        resultReducer: (state, action) => {
+            state.result = action.payload;
+        },
+        errorReducer: (state, action) => {
+            state.error = action.payload;
+        },
+
+    }
+})
+
+function getToken() {
+    return localStorage.getItem("Authorization");
+}
+function getRole() {
+    return localStorage.getItem("role");
+}
+
+export const addRegionDepartment = (data) => apiCall({
+    url: "/regionDepartment",
+    method: "POST",
+    headers: {
+        Authorization: getToken(),
+        role: getRole(),
+    },
+    data,
+    success: slice.actions.resultReducer.type,
+    error: slice.actions.errorReducer.type
+})
+
+export const tokenNull = ()=>{
+    return {
+        type:slice.actions.tokenReducer.type,
+        payload:null
+    }
+}
+
+
+
+
+export default slice.reducer;
